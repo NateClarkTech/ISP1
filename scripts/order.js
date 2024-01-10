a09f6d0f9ed30c6f39833676e57cb2d8d2acdb19
NateClarkTech <NateClarkTech@gmail.com>
2024-01-10 17:35:06 -0500

    Finished menu and contact page. Also added some stuff to order page.

--- scripts/order.js
+++ scripts/order.js
@@ -3,3 +3,20 @@ function clearRadioForm(id) {
     var form = document.getElementById(id);
     form.reset();
 }
+
+function orderPizza(idRadio, idCheckbox) {
+    var size = document.getElementById(idRadio)
+    size.reset();
+
+    var toppings = document.getElementById(idCheckbox)
+    for (var i = 0; i < toppings.length; i++){
+        if (toppings[i].checked){
+            console.log(toppings[i]);
+            toppings[i].checked = false;
+        }
+    }
+}
+
+function clearForm(idForm){
+
+}
